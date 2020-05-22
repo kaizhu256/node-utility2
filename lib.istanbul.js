8e5332622094badab07a1a680a23cec861728246
kai.zhu <kai.zhu@windebt.com>
2020-05-21 19:01:42 -0500

    inline.reportTextCreate

--- lib.istanbul.js
+++ lib.istanbul.js
@@ -12174,13 +12174,6 @@ file https://github.com/gotwarlost/istanbul/blob/v0.2.16/lib/report/text.js
  Copyrights licensed under the New BSD License. See the accompanying LICENSE file for terms.
  */
 
-var defaults = require('./common/defaults'),
-    utils = require('../object-utils'),
-    PCT_COLS = 10,
-    TAB_SIZE = 3,
-    DELIM = ' |',
-    COL_DELIM = '-|';
-
 /**
  * a `Report` implementation that produces text output in a detailed table.
  *
@@ -12199,6 +12192,133 @@ var defaults = require('./common/defaults'),
  *              to be reported.
  */
 local.reportTextCreate = function(opts, collector) {
+    var defaults = require('./common/defaults'),
+        utils = require('../object-utils'),
+        PCT_COLS = 10,
+        TAB_SIZE = 3,
+        DELIM = ' |',
+        COL_DELIM = '-|';
+
+    function padding(num, ch) {
+        var str = '',
+            i;
+        ch = ch || ' ';
+        for (i = 0; i < num; i += 1) {
+            str += ch;
+        }
+        return str;
+    }
+
+    function fill(str, width, right, tabs, clazz) {
+        tabs = tabs || 0;
+        str = String(str);
+
+        var leadingSpaces = tabs * TAB_SIZE,
+            remaining = width - leadingSpaces,
+            leader = padding(leadingSpaces),
+            fmtStr = '',
+            fillStr,
+            strlen = str.length;
+
+        if (remaining > 0) {
+            if (remaining >= strlen) {
+                fillStr = padding(remaining - strlen);
+                fmtStr = right ? fillStr + str : str + fillStr;
+            } else {
+                fmtStr = str.substring(strlen - remaining);
+                fmtStr = '... ' + fmtStr.substring(4);
+            }
+        }
+
+        fmtStr = defaults.colorize(fmtStr, clazz);
+        return leader + fmtStr;
+    }
+
+    function formatName(name, maxCols, level, clazz) {
+        return fill(name, maxCols, false, level, clazz);
+    }
+
+    function formatPct(pct, clazz) {
+        return fill(pct, PCT_COLS, true, 0, clazz);
+    }
+
+    function nodeName(node) {
+        return node.displayShortName() || 'All files';
+    }
+
+    function tableHeader(maxNameCols) {
+        var elements = [];
+        elements.push(formatName('File', maxNameCols, 0));
+        elements.push(formatPct('% Stmts'));
+        elements.push(formatPct('% Branches'));
+        elements.push(formatPct('% Funcs'));
+        elements.push(formatPct('% Lines'));
+        return elements.join(' |') + ' |';
+    }
+
+    function tableRow(node, maxNameCols, level, watermarks) {
+        var name = nodeName(node),
+            statements = node.metrics.statements.pct,
+            branches = node.metrics.branches.pct,
+            functions = node.metrics.functions.pct,
+            lines = node.metrics.lines.pct,
+            elements = [];
+
+        elements.push(formatName(name, maxNameCols, level, defaults.classFor('statements', node.metrics, watermarks)));
+        elements.push(formatPct(statements, defaults.classFor('statements', node.metrics, watermarks)));
+        elements.push(formatPct(branches, defaults.classFor('branches', node.metrics, watermarks)));
+        elements.push(formatPct(functions, defaults.classFor('functions', node.metrics, watermarks)));
+        elements.push(formatPct(lines, defaults.classFor('lines', node.metrics, watermarks)));
+
+        return elements.join(DELIM) + DELIM;
+    }
+
+    function findNameWidth(node, level, last) {
+        last = last || 0;
+        level = level || 0;
+        var idealWidth = TAB_SIZE * level + nodeName(node).length;
+        if (idealWidth > last) {
+            last = idealWidth;
+        }
+        node.children.forEach(function (child) {
+            last = findNameWidth(child, level + 1, last);
+        });
+        return last;
+    }
+
+    function makeLine(nameWidth) {
+        var name = padding(nameWidth, '-'),
+            pct = padding(PCT_COLS, '-'),
+            elements = [];
+
+        elements.push(name);
+        elements.push(pct);
+        elements.push(pct);
+        elements.push(pct);
+        elements.push(pct);
+        return elements.join(COL_DELIM) + COL_DELIM;
+    }
+
+    function walk(node, nameWidth, array, level, watermarks) {
+        var line;
+        if (level === 0) {
+            line = makeLine(nameWidth);
+            array.push(line);
+            array.push(tableHeader(nameWidth));
+            array.push(line);
+        } else {
+            array.push(tableRow(node, nameWidth, level, watermarks));
+        }
+        node.children.forEach(function (child) {
+            walk(child, nameWidth, array, level + 1, watermarks);
+        });
+        if (level === 0) {
+            array.push(line);
+            array.push(tableRow(node, nameWidth, level, watermarks));
+            array.push(line);
+        }
+    }
+
     var that;
     that = {};
     opts = opts || {};
@@ -12226,126 +12346,6 @@ local.reportTextCreate = function(opts, collector) {
     text = strings.join('\n') + '\n';
     console.log(text);
 };
-
-function padding(num, ch) {
-    var str = '',
-        i;
-    ch = ch || ' ';
-    for (i = 0; i < num; i += 1) {
-        str += ch;
-    }
-    return str;
-}
-
-function fill(str, width, right, tabs, clazz) {
-    tabs = tabs || 0;
-    str = String(str);
-
-    var leadingSpaces = tabs * TAB_SIZE,
-        remaining = width - leadingSpaces,
-        leader = padding(leadingSpaces),
-        fmtStr = '',
-        fillStr,
-        strlen = str.length;
-
-    if (remaining > 0) {
-        if (remaining >= strlen) {
-            fillStr = padding(remaining - strlen);
-            fmtStr = right ? fillStr + str : str + fillStr;
-        } else {
-            fmtStr = str.substring(strlen - remaining);
-            fmtStr = '... ' + fmtStr.substring(4);
-        }
-    }
-
-    fmtStr = defaults.colorize(fmtStr, clazz);
-    return leader + fmtStr;
-}
-
-function formatName(name, maxCols, level, clazz) {
-    return fill(name, maxCols, false, level, clazz);
-}
-
-function formatPct(pct, clazz) {
-    return fill(pct, PCT_COLS, true, 0, clazz);
-}
-
-function nodeName(node) {
-    return node.displayShortName() || 'All files';
-}
-
-function tableHeader(maxNameCols) {
-    var elements = [];
-    elements.push(formatName('File', maxNameCols, 0));
-    elements.push(formatPct('% Stmts'));
-    elements.push(formatPct('% Branches'));
-    elements.push(formatPct('% Funcs'));
-    elements.push(formatPct('% Lines'));
-    return elements.join(' |') + ' |';
-}
-
-function tableRow(node, maxNameCols, level, watermarks) {
-    var name = nodeName(node),
-        statements = node.metrics.statements.pct,
-        branches = node.metrics.branches.pct,
-        functions = node.metrics.functions.pct,
-        lines = node.metrics.lines.pct,
-        elements = [];
-
-    elements.push(formatName(name, maxNameCols, level, defaults.classFor('statements', node.metrics, watermarks)));
-    elements.push(formatPct(statements, defaults.classFor('statements', node.metrics, watermarks)));
-    elements.push(formatPct(branches, defaults.classFor('branches', node.metrics, watermarks)));
-    elements.push(formatPct(functions, defaults.classFor('functions', node.metrics, watermarks)));
-    elements.push(formatPct(lines, defaults.classFor('lines', node.metrics, watermarks)));
-
-    return elements.join(DELIM) + DELIM;
-}
-
-function findNameWidth(node, level, last) {
-    last = last || 0;
-    level = level || 0;
-    var idealWidth = TAB_SIZE * level + nodeName(node).length;
-    if (idealWidth > last) {
-        last = idealWidth;
-    }
-    node.children.forEach(function (child) {
-        last = findNameWidth(child, level + 1, last);
-    });
-    return last;
-}
-
-function makeLine(nameWidth) {
-    var name = padding(nameWidth, '-'),
-        pct = padding(PCT_COLS, '-'),
-        elements = [];
-
-    elements.push(name);
-    elements.push(pct);
-    elements.push(pct);
-    elements.push(pct);
-    elements.push(pct);
-    return elements.join(COL_DELIM) + COL_DELIM;
-}
-
-function walk(node, nameWidth, array, level, watermarks) {
-    var line;
-    if (level === 0) {
-        line = makeLine(nameWidth);
-        array.push(line);
-        array.push(tableHeader(nameWidth));
-        array.push(line);
-    } else {
-        array.push(tableRow(node, nameWidth, level, watermarks));
-    }
-    node.children.forEach(function (child) {
-        walk(child, nameWidth, array, level + 1, watermarks);
-    });
-    if (level === 0) {
-        array.push(line);
-        array.push(tableRow(node, nameWidth, level, watermarks));
-        array.push(line);
-    }
-}
 }());
 
 
