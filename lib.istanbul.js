cf44f1b5eae4b3d825b2433a3f0e6f9a2abb3ef0
kai zhu <kaizhu256@gmail.com>
2020-05-21 21:21:18 -0500

    file.none

--- lib.istanbul.js
+++ lib.istanbul.js
@@ -11657,12 +11657,6 @@ file https://img.shields.io/badge/coverage-100.0%-00dd00.svg?style=flat
 */
 local.templateCoverageBadgeSvg =
 '<svg xmlns="http://www.w3.org/2000/svg" width="117" height="20"><linearGradient id="a" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><rect rx="0" width="117" height="20" fill="#555"/><rect rx="0" x="63" width="54" height="20" fill="#0d0"/><path fill="#0d0" d="M63 0h4v20h-4z"/><rect rx="0" width="117" height="20" fill="url(#a)"/><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11"><text x="32.5" y="15" fill="#010101" fill-opacity=".3">coverage</text><text x="32.5" y="14">coverage</text><text x="89" y="15" fill="#010101" fill-opacity=".3">100.0%</text><text x="89" y="14">100.0%</text></g></svg>';
-
-
-
-/*
-file none
-*/
 /* jslint ignore:end */
 }());
 
@@ -12680,6 +12674,10 @@ local.reportTextCreate = function (opts, collector) {
     text = strings.join("\n") + "\n";
     console.log(text);
 };
+
+/*
+file none
+*/
 
 
 
