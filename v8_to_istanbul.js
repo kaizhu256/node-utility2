b0e5a75725673d32be8a28d53e1da2c9a034a09b
kai zhu <kaizhu256@gmail.com>
2021-05-16 01:04:44 -0500

    originalPositionFor

--- v8_to_istanbul.js
+++ v8_to_istanbul.js
@@ -142,3 +142,93 @@ function offsetToOriginalRelative(sourceMap, startCol, endCol) {
     };
 }
 offsetToOriginalRelative();
+
+
+/**
+ * Returns the original source, line, and column information for the generated
+ * source's line and column positions provided. The only argument is an object
+ * with the following properties:
+ *
+ *   - line: The line number in the generated source.  The line number
+ *     is 1-based.
+ *   - column: The column number in the generated source.  The column
+ *     number is 0-based.
+ *   - bias: Either 'SourceMapConsumer.GREATEST_LOWER_BOUND' or
+ *     'SourceMapConsumer.LEAST_UPPER_BOUND'. Specifies whether to return the
+ *     closest element that is smaller than or greater than the one we are
+ *     searching for, respectively, if the exact element cannot be found.
+ *     Defaults to 'SourceMapConsumer.GREATEST_LOWER_BOUND'.
+ *
+ * and an object is returned with the following properties:
+ *
+ *   - source: The original source file, or null.
+ *   - line: The line number in the original source, or null.  The
+ *     line number is 1-based.
+ *   - column: The column number in the original source, or null.  The
+ *     column number is 0-based.
+ *   - name: The original identifier, or null.
+ */
+function originalPositionFor(aArgs) {
+    const needle = {
+        generatedLine: util.getArg(aArgs, "line"),
+        generatedColumn: util.getArg(aArgs, "column")
+    };
+
+    if (needle.generatedLine < 1) {
+        throw new Error("Line numbers must be >= 1");
+    }
+
+    if (needle.generatedColumn < 0) {
+        throw new Error("Column numbers must be >= 0");
+    }
+
+    let bias = util.getArg(
+        aArgs,
+        "bias",
+        SourceMapConsumer.GREATEST_LOWER_BOUND
+    );
+    if (bias == null) {
+        bias = SourceMapConsumer.GREATEST_LOWER_BOUND;
+    }
+
+    let mapping;
+    this._wasm.withMappingCallback(
+        m => (mapping = m),
+        () => {
+            this._wasm.exports.original_location_for(
+                this._getMappingsPtr(),
+                needle.generatedLine - 1,
+                needle.generatedColumn,
+                bias
+            );
+        }
+    );
+
+    if (mapping) {
+        if (mapping.generatedLine === needle.generatedLine) {
+            let source = util.getArg(mapping, "source", null);
+            if (source !== null) {
+                source = this._absoluteSources.at(source);
+            }
+
+            let name = util.getArg(mapping, "name", null);
+            if (name !== null) {
+                name = this._names.at(name);
+            }
+
+            return {
+                source,
+                line: util.getArg(mapping, "originalLine", null),
+                column: util.getArg(mapping, "originalColumn", null),
+                name
+            };
+        }
+    }
+
+    return {
+        source: null,
+        line: null,
+        column: null,
+        name: null
+    };
+}
